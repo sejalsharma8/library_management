7f8a8169e278f126bb63dce91c02e6cbaffbb606
sejalsharma8 <139431987+sejalsharma8@users.noreply.github.com>
2023-07-13 22:13:08 +0530

    Update Constant.java

--- src/main/java/com/nagarro/library_management/constants/Constant.java
+++ src/main/java/com/nagarro/library_management/constants/Constant.java
@@ -4,7 +4,7 @@ public class Constant {
 	public static final String HIBERNATE_USERNAME="sejal";
 	public static final String HIBERNATE_PASSWORD="sharma8#";
 	public static final String HIBERNATE_DIALECT="org.hibernate.dialect.MySQLDialect";
-	//public static final String HIBERNATE_DRIVER="com.mysql.cj.jdbc.Driver";
+	public static final String HIBERNATE_DRIVER="com.mysql.cj.jdbc.Driver";
 	public static final String HIBERNATE_URL="jdbc:mysql://springmvcdb.mysql.database.azure.com:3306/library_login";
 	public static final String HIBERNATE_HBM2DDL_AUTO="update";
 	public static final String HIBERNATE_SHOW_SQL="false";
