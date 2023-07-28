6bc7d618b91a381bcc5bea49feddcbd27c4dfb8a
sejalsharma8 <139431987+sejalsharma8@users.noreply.github.com>
2023-07-28 12:13:12 +0530

    Update Constant.java

--- src/main/java/com/nagarro/library_management/constants/Constant.java
+++ src/main/java/com/nagarro/library_management/constants/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
 	public static final String HIBERNATE_PASSWORD="sejalsharma";
 	public static final String HIBERNATE_DIALECT="org.hibernate.dialect.MySQLDialect";
 	public static final String HIBERNATE_DRIVER="com.mysql.cj.jdbc.Driver";
-	public static final String HIBERNATE_URL="jdbc:mysql://database-1.cnjixg3clbau.ap-southeast-1.rds.amazonaws.com:3306/library_login";
+	public static final String HIBERNATE_URL="jdbc:mysql://library.cnjixg3clbau.ap-southeast-1.rds.amazonaws.com:3306/library_login";
 	public static final String HIBERNATE_HBM2DDL_AUTO="update";
 	public static final String HIBERNATE_SHOW_SQL="false";
 	public static final String BOOK_API = "http://localhost:8082/api/books";
